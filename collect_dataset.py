56f6ca4c78cfbe30247075e7de95b3408ebcadb4
Mario Negovetić <mario.negovetic1@gmail.com>
2024-11-13 10:09:29 +0100

    parantheses and question mark

--- collect_dataset.py
+++ collect_dataset.py
@@ -23,28 +23,22 @@ def create_custom_dataset():
 
         print('Collecting data for class {}'.format(class_names[j]))
 
-        done = False
-        while True:
-            ret, frame = cap.read()
-            cv2.putText(frame, None, (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
-                        cv2.LINE_AA)
-            cv2.imshow('frame', frame)
-            if cv2.waitKey(25) == ord('q'):
-                break
-
         counter = 0
         while counter < dataset_size:
             ret, frame = cap.read()
+            cv2.putText(frame, None,
+                        (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2, cv2.LINE_AA)
             cv2.imshow('frame', frame)
-            cv2.waitKey(25)
 
-            image_name = '{}{}.jpg'.format(class_names[j], counter)
-            cv2.imwrite(os.path.join(class_dir, image_name), frame)
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord(' '):  # Press SPACE to save the image
+                image_name = '{}{}.jpg'.format(class_names[j], counter)
+                cv2.imwrite(os.path.join(class_dir, image_name), frame)
+                print(f"Saved {image_name}")
 
-            counter += 1
+                counter += 1
 
     cap.release()
     cv2.destroyAllWindows()
-
 if __name__ == '__main__':
     create_custom_dataset()
