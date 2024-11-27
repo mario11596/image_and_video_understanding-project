257902db085909973f3b4df6bac069fb7b0208f5
Mario Negovetić <mario.negovetic1@gmail.com>
2024-11-27 18:29:14 +0100

    modify scipt to create test set

--- dataset_preparing.py
+++ dataset_preparing.py
@@ -43,23 +43,31 @@ def combine_datasets():
 def split_dataset():
     train_directory = r'.\dataset\train'
     validation_directory = r'.\dataset\validation'
+    test_directory = r'.\dataset\test'
     source_base = r'.\1\asl_alphabet_train\asl_alphabet_train'
 
     os.makedirs(train_directory, exist_ok=True)
     os.makedirs(validation_directory, exist_ok=True)
+    os.makedirs(test_directory, exist_ok=True)
 
     for folder_name in folder_names:
         source_folder = os.path.join(source_base, folder_name)
         train_folder = os.path.join(train_directory, folder_name)
         validation_folder = os.path.join(validation_directory, folder_name)
+        test_folder = os.path.join(test_directory, folder_name)
 
         images = [os.path.join(source_folder, img) for img in os.listdir(source_folder) if
                   img.lower().endswith(('.png', '.jpg', '.jpeg'))]
 
-        train_images, validation_images = train_test_split(images, train_size=0.8, random_state=302)
+        # Split images into train + test (80% train, 20% test)
+        train_images, test_images = train_test_split(images, train_size=0.8, random_state=302)
+
+        # Split the train set into train and validation sets (80% train, 20% validation)
+        train_images, validation_images = train_test_split(train_images, train_size=0.8, random_state=302)
 
         os.makedirs(train_folder, exist_ok=True)
         os.makedirs(validation_folder, exist_ok=True)
+        os.makedirs(test_folder, exist_ok=True)
 
         for img in train_images:
             shutil.copy(img, train_folder)
@@ -67,9 +75,12 @@ def split_dataset():
         for img in validation_images:
             shutil.copy(img, validation_folder)
 
+        for img in test_images:
+            shutil.copy(img, test_folder)
+
 
 if __name__ == '__main__':
-    path_kaggle = download_kaggle_dataset()
-    copy_dataset(path_kaggle)
-    combine_datasets()
+    #path_kaggle = download_kaggle_dataset()
+    #copy_dataset(path_kaggle)
+    #combine_datasets()
     split_dataset()
